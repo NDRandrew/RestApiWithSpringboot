25539e25769bc7884e36db5c2169c69fdc037d9f
Andrew <50187249+NDRandrew@users.noreply.github.com>
2023-07-10 13:41:26 -0300

    Add files via upload

---
+++ mysql-Script.sql
@@ -0,0 +1,9 @@
+use oneclickbuy;
+create table oneclickbuy(
+	name varchar(255),
+    price int not null
+);
+select * from oneclickbuy_db;
+alter table oneclickbuy_db
+drop column codigo_seguranca;
+
